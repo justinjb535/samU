c160013a2f97e320dacc50799fb67d37c43486c6
justinjb535 <justinjb535@users.noreply.github.com>
2026-09-15 22:40:54 +0200

    uncommented a typo

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 app.use('/uploads', express.static('uploads'));
-app.use('/api/auth', signupRouter);
+//app.use('/api/auth', signupRouter);
 
 let pdfText = "";  // full text for TTS
 let pdfDoc = null; // pdfjs doc for paging
